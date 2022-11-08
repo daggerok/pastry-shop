f0f01e6770b854a803393b71fa6a45865ba9297d
Maksim Kostromin <daggerok@gmail.com>
2022-11-08 22:08:11 +0200

    Use react state to manage strikethrough functionality.

--- src/app/components/TodoApp/ListItems/Task.js
+++ src/app/components/TodoApp/ListItems/Task.js
@@ -1,2 +1,15 @@
-export const Task = ({ description }) =>
-    <li>{description}</li>;
+import React, { useState } from 'react';
+
+export const Task = ({ description }) => {
+    const [ isCrossed, setIsCrossed ] = useState(false);
+    const [ taskStyles, setTaskStyles ] = useState({ textDecoration: 'none '});
+
+    const crossText = () => {
+        setIsCrossed(!isCrossed);
+        const textDecoration = isCrossed ? 'line-through' : 'none';
+        setTaskStyles({ textDecoration: textDecoration });
+    };
+    return <li style={taskStyles} onClick={crossText}>
+        {description}
+    </li>;
+}
